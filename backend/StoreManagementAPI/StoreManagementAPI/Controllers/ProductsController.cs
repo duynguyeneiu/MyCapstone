1fa3c3d8a83e78744b1e8598421ce65e7d8bf3a4
duynguyeneiu <155942227+duynguyeneiu@users.noreply.github.com>
2026-07-28 18:11:36 +0700

    Catalog service

---
+++ backend/StoreManagementAPI/StoreManagementAPI/Controllers/ProductsController.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreManagementAPI.Models;
+using StoreManagementAPI.Data;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly CatalogContext _context;
+    public ProductsController(CatalogContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Product
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    // GET: api/Product/5
+    [HttpGet("{productid}")]
+    public async Task<ActionResult<Product>> GetProduct(int productid)
+    {
+        var product = await _context.Products.FindAsync(productid);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return product;
+    }
+
+    // PUT: api/Product/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{productid}")]
+    public async Task<IActionResult> PutProduct(int? productid, Product product)
+    {
+        if (productid != product.ProductId)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(product).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ProductExists(productid))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Product
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Product>> PostProduct(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetProduct", new { productid = product.ProductId }, product);
+    }
+
+    // DELETE: api/Product/5
+    [HttpDelete("{productid}")]
+    public async Task<IActionResult> DeleteProduct(int? productid)
+    {
+        var product = await _context.Products.FindAsync(productid);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool ProductExists(int? productid)
+    {
+        return _context.Products.Any(e => e.ProductId == productid);
+    }
+}
---
+++ backend/StoreManagementAPI/StoreManagementAPI/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace StoreManagementAPI.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int? ParentCategoryId { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+    [JsonIgnore]
+    public virtual ICollection<Category> InverseParentCategory { get; set; } = new List<Category>();
+
+    public virtual Category? ParentCategory { get; set; }
+    [JsonIgnore]
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
